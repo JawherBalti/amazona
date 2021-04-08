2e33d68c582bc53791f6c7162e5b4cfbf73a9a14
JawherBalti <jawher-balti@live.com>
2021-04-08 18:09:52 +0100

    product detail route added

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ app.get("/api/products", (req, res) => {
 app.get("/api/products/:id", (req, res) => {
     const product = data.products.find(index => index._id === req.params.id)
     if(product) res.send(product)
-    else res.status(404).send({message: "Product not found"})
+    else res.status(404).send({message: "Product not found!"})
 })
 
 const port = process.env.PORT || 5000
