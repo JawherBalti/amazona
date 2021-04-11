e041b0bf42a5de8e3a0f5cb29a4ada64768d8877
JawherBalti <jawher-balti@live.com>
2021-04-11 22:27:01 +0100

    user model added

---
+++ backend/models/user.js
@@ -0,0 +1,84 @@
+const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+const moment = require('moment')
+
+const saltRounds = 10;
+
+const UserSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        lowercase: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    isAdmin: {
+        type: Boolean,
+        default: false
+    },
+    token: {
+        type: String,
+    },
+    tokenExp: {
+        type: Number
+    }
+})
+
+UserSchema.pre('save', function (next) {
+    var user = this;
+    if (user.isModified('password')) {
+        bcrypt.genSalt(saltRounds, function (err, salt) {
+            if (err) return next(err);
+            bcrypt.hash(user.password, salt, function (err, hash) {
+                if (err) return next(err);
+                user.password = hash
+                next()
+            })
+        })
+    } else {
+        next()
+    }
+});
+
+UserSchema.methods.comparePassword = function (plainPassword, cb) {
+    bcrypt.compare(plainPassword, this.password, function (err, isMatch) {
+        if (err) return cb(err);
+        cb(null, isMatch)
+    })
+}
+
+UserSchema.methods.generateToken = function (cb) {
+    var user = this;
+    var token = jwt.sign(user._id.toHexString(), process.env.SECRET)
+    var oneHour = moment().add(1, 'hour').valueOf();
+
+    user.tokenExp = oneHour;
+    user.token = token;
+    user.save(function (err, user) {
+        if (err) return cb(err)
+        cb(null, user);
+    })
+}
+
+UserSchema.statics.findByToken = function (token, cb) {
+    var user = this;
+
+    jwt.verify(token, 'secret', function (err, decode) {
+        user.findOne({ "_id": decode, "token": token }, function (err, user) {
+            if (err) return cb(err);
+            cb(null, user);
+        })
+    })
+}
+
+const User = mongoose.model("User", UserSchema)
+module.exports = { User }
