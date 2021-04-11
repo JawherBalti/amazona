efff47b7294c09d0726c84621c8b43a32c88f542
JawherBalti <jawher-balti@live.com>
2021-04-11 22:30:33 +0100

    added express json parser

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,37 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+const userRoute = require('./routes/user')
 const data = require('./data')
 
+dotenv.config()
+
 const app = express()
+app.use(express.json())
+
+app.use('/api/user', userRoute)
+
 app.get("/api/products", (req, res) => {
     res.send(data.products)
 })
 
 app.get("/api/products/:id", (req, res) => {
     const product = data.products.find(index => index._id === req.params.id)
-    if(product) res.send(product)
-    else res.status(404).send({message: "Product not found!"})
+    if (product) res.send(product)
+    else res.status(404).send({ message: "Product not found!" })
 })
 
 const port = process.env.PORT || 5000
+const db_uri = process.env.MONGO_URI
+
+mongoose.connect(db_uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+})
+mongoose.connection.once("open", () => {
+    console.log("Connection to database established")
+})
 
 app.listen(port, () => console.log(`server is running on port ${port}`))
