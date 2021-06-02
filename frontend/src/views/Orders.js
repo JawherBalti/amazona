db717784f13de4aa4fb409fae380babfc2990631
JawherBalti <jawher-balti@live.com>
2021-06-02 21:54:30 +0100

    added pagination to orders

--- frontend/src/views/Orders.js
+++ frontend/src/views/Orders.js
@@ -1,24 +1,67 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import LoadingBox from '../components/LoadingBox'
 import MessageBox from '../components/MessageBox'
-import { allOrderss } from '../actions/order'
+import { allOrderss, deleteOrder } from '../actions/order'
+import Pagination from '../components/Pagination'
 
 export default function Orders(props) {
     const allOrderList = useSelector(state => state.allOrdersReducer)
     const { loading, error, allOrders } = allOrderList
-
     const userSignIn = useSelector(state => state.userSignInReducer)
     const { userInfo } = userSignIn
+
+    const [orders, setOrders] = useState([])
+    const [currentPage, setCurrentPage] = useState(1)
+    const [ordersPerPage, setOrdersPerPage] = useState(9)
+
+    const [pageNumberLimit, setpageNumberLimit] = useState(5)
+    const [maxPageNumberLimit, setmaxPageNumberLimit] = useState(5)
+    const [minPageNumberLimit, setminPageNumberLimit] = useState(0)
+
+    const lastOrder = currentPage * ordersPerPage
+    const firstOrder = lastOrder - ordersPerPage
 
     if (!userInfo) {
         props.history.push("/signin")
     }
 
     const dispatch = useDispatch()
+
     useEffect(() => {
         dispatch(allOrderss())
-    }, [dispatch])
+        if (allOrders) {
+            setOrders(allOrders)
+        }
+    }, [dispatch, orders]) //add order so that component rerenders on order delete
+    
+    const deleteHandler = (orderId) => {
+        dispatch(deleteOrder(orderId))
+        setOrders(orders.filter(order => order._id !== orderId))
+    }
+
+    const paginate = (pageNumber) => {
+        setCurrentPage(pageNumber)
+    }
+
+    const handleNextbtn = () => {
+        setCurrentPage(currentPage + 1);
+
+        if (currentPage + 1 > maxPageNumberLimit) {
+            setmaxPageNumberLimit(maxPageNumberLimit + pageNumberLimit);
+            setminPageNumberLimit(minPageNumberLimit + pageNumberLimit);
+        }
+    }
+
+    const handlePrevbtn = () => {
+        setCurrentPage(currentPage - 1);
+
+        if ((currentPage - 1) % pageNumberLimit === 0) {
+            setmaxPageNumberLimit(maxPageNumberLimit - pageNumberLimit);
+            setminPageNumberLimit(minPageNumberLimit - pageNumberLimit);
+        }
+    }
+
     return (
         <div>
             <div>Order history</div>
@@ -36,21 +79,24 @@ export default function Orders(props) {
                             </tr>
                         </thead>
                         <tbody>
-                            {allOrders.map(order => (
+                            {allOrders.slice(firstOrder, lastOrder).map(order => (
                                 <tr key={order._id}>
                                     <td>{order._id}</td>
-                                    <td>{order.createdAt}</td>
+                                    <td>{order.createdAt.substring(0, 10)}</td>
                                     <td>{order.totalPrice.toFixed(2)}</td>
                                     <td>{order.isPaid ? order.paidAt.substring(0, 10) : "Not Paid"}</td>
                                     <td>{order.isDelivered ? order.deliveredAt.substring(0, 10) : "Not Delivered"}</td>
                                     <td>
-                                        <button type="button" className="small" onClick={() => { props.history.push(`/order/${order._id}`) }}>Details</button>
-                                        <button type="button" className="small" onClick={() => { props.history.push(`/order/${order._id}`) }}>Delete</button>
+                                        <button type="button" className="small" onClick={() => { props.history.push(`/order/deliver/${order._id}`) }}>Details</button>
+                                        <button type="button" className="small" onClick={() => deleteHandler(order._id)}>Delete</button>
                                     </td>
                                 </tr>
                             ))}
                         </tbody>
                     </table>}
+            {
+                <Pagination ordersPerPage={ordersPerPage} totalOrders={allOrders ? allOrders.length : orders.length} paginate={paginate} currentPage={currentPage} maxPageNumberLimit={maxPageNumberLimit} minPageNumberLimit={minPageNumberLimit} handleNextbtn={handleNextbtn} handlePrevbtn={handlePrevbtn}></Pagination>
+            }
         </div>
     )
 }
